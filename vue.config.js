5bbd847e604caff9c0ad187d081f99d2cda48c73
daniel@duton.lu <daniel@duton.lu>
2022-06-24 22:25:50 +0200

    explicit set localIdentName fixes the issue

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,13 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  css: {
+    loaderOptions: {
+      css: {
+        modules: {
+          localIdentName: '[name]_[local]_[hash:base64:5]'
+        }
+      }
+    }
+  }
 })
